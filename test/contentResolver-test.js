2b931897dff4d6c6d8945780917fc909f64a4961
Stephen Lynn <stephen.g.lynn@gmail.com>
2012-11-21 11:48:34 -0700

    Add check for css file local to module folder and include load call if present.

--- test/contentResolver-test.js
+++ test/contentResolver-test.js
@@ -71,6 +71,12 @@ buster.testCase("contentResolver tests", {
       assert.equals(path.resolve("test/app1/js/fullModule/assembly.json"), js.getDiskPath());
       assert.equals("//Module assembly: fullModule\n\n/*\n * Included File: helpers.js\n */\n\nvar h=\"helper.js\";\n\n/*\n * Included File: main.js\n */\n\nvar m=\"main.js\";\n\n/*\n * Included File: fullModule_zh.json\n */\n\nvar lang = {\"title\":\"value\",\"onlyEN\":\"value\"};\n\n/*\n * Included File: template.html\n */\n\n\nvar snippetsRaw = \"\\n\" + \n\"    html template body\\n\" + \n\"\\n\" + \n\"\";\n\n\nfunction getSnippets(){\nvar snip = document.createElement('div');\n$(snip).html(snippetsRaw.format(lang));\n\nreturn snip;\n}\n\n\n/*\n * Included File: CSS auto import\n */\n\nloadCSS('fullModule.css');\n\n", js.getContent());
     },
+    
+    "for a fullModuleWithCSS": function() {
+      var js = this.cf("", "fullModuleWithCSS", "js", "js");
+      assert.equals(path.resolve("test/app1/js/fullModuleWithCSS/assembly.json"), js.getDiskPath());
+      assert.equals("//Module assembly: fullModuleWithCSS\n\n/*\n * Included File: helpers.js\n */\n\nvar h=\"helper.js\";\n\n/*\n * Included File: main.js\n */\n\nvar m=\"main.js\";\n\n/*\n * Included File: fullModuleWithCSS_en.json\n */\n\nvar lang = {\"title\":\"value\",\"onlyEN\":\"value\"};\n\n/*\n * Included File: template.html\n */\n\n\nvar snippetsRaw = \"\\n\" + \n\"    html template body\\n\" + \n\"\\n\" + \n\"\";\n\n\nfunction getSnippets(){\nvar snip = document.createElement('div');\n$(snip).html(snippetsRaw.format(lang));\n\nreturn snip;\n}\n\n\n/*\n * Included File: CSS auto import\n */\n\nloadCSS('fullModuleWithCSS.css');\n\n", js.getContent());
+    },
 
     "for a Module with spread out parts (Uses Asset Resolution Paths)": function() {
       var js = this.cf("", "testModule", "js", "js");
